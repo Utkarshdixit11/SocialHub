aebfd674ae90f35b5a1db2ff9fcfd0879e065b10
Utkarshdixit11 <utkarshdixit925@gmail.com>
2025-08-04 01:21:35 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // TODO: move this to env file
-const API_BASE_URL = 'http://localhost:4000'
+const API_BASE_URL = 'https://socialhub-backend-wmce.onrender.com'
 
 // setup axios instance
 const apiClient = axios.create({
